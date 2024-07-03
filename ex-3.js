998d5193cc8072ac2ddcffff13196bb88207f231
kornkamon view <kornkamon.lert@gmail.com>
2024-07-03 16:23:27 +0700

    feat: Add algo-nested-loop

--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,17 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(number) {
+  // console.log(number);
+  let finalResult = "";
+  for (let i = 1; i <= number; i++) {
+    //console.log(i);
+    let rowResult = "";
+    for (let j = 1; j <= number; j++) {
+      //console.log(j);
+      rowResult += j * i + "\t";
+      //console.log(rowResult);
+    }
+    finalResult += rowResult + "\n";
+  }
+  return finalResult;
 }
+console.log(multiplicationTable(5));
